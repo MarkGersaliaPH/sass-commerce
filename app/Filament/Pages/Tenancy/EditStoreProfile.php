0b91e2e78313a257694f0197ee66e887bdda9809
Mark Anthony Gersalia <markgersalia.codev@gmail.com>
2024-06-01 12:25:34 +0800

    Add seller is open function

--- app/Filament/Pages/Tenancy/EditStoreProfile.php
+++ app/Filament/Pages/Tenancy/EditStoreProfile.php
@@ -5,8 +5,10 @@ namespace App\Filament\Pages\Tenancy;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
 use Filament\Forms\Form;
 use Filament\Pages\Tenancy\EditTenantProfile;
+use Filament\Tables\Columns\ToggleColumn;
 
 class EditStoreProfile extends EditTenantProfile
 {
@@ -25,6 +27,7 @@ class EditStoreProfile extends EditTenantProfile
                         ->imageEditor()
                         ->directory('stores')
                         ->circleCropper(),
+                        Toggle::make("is_open")->default(true),
                     TextInput::make('name')
                         ->required()
                         ->maxLength(255),
